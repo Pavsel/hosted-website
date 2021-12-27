8fc382731d078e0ce2080afae6bac6cd646c80b6
pavel <pashaivanov012@gmail.com>
2021-12-26 19:43:06 -0500

    changed homepage to use custom url

--- src/components/Art/Art.jsx
+++ src/components/Art/Art.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import './art.css';
 import ButtonBlack from "../button/button-black/ButtonBlack";
 
+import Img01 from "../../assets/Art Grid/01.png"
+import Img02 from "../../assets/Art Grid/02.png"
+import Img03 from "../../assets/Art Grid/03.png"
+import Img04 from "../../assets/Art Grid/04.png"
+import Img05 from "../../assets/Art Grid/05.png"
+import Img06 from "../../assets/Art Grid/06.png"
+import Img07 from "../../assets/Art Grid/07.png"
+import Img08 from "../../assets/Art Grid/08.png"
+import Img09 from "../../assets/Art Grid/09.png"
+import Img10 from "../../assets/Art Grid/10.png"
+import Img11 from "../../assets/Art Grid/11.png"
+import Img12 from "../../assets/Art Grid/12.png"
+import Img13 from "../../assets/Art Grid/13.png"
 
 function Art() {
     return (
         <section className="art-section">
             <div className="art-container">
                 <h1>3D Art</h1>
-                <p>Starting from assets for my games, I got into the world of 3D art. <b>I have over 4 years of expirience
+                <p>Starting from assets for my games, I got into the world of 3D art. <b>I have over 4 years of
+                    expirience
                     using Blender 3D: modelling, sculpting, animation, making materials, lighting ect.</b>
                     <br/>
                     <br/>
@@ -17,6 +31,22 @@ function Art() {
                     text="Instagram"
                     btnLink="https://www.instagram.com/pav3d/"
                 />
+            </div>
+            <div className="gallery">
+                <h1>More Work</h1>
+                <img src={Img01}/>
+                <img src={Img02}/>
+                <img src={Img03}/>
+                <img src={Img04}/>
+                <img src={Img05}/>
+                <img src={Img06}/>
+                <img src={Img07}/>
+                <img src={Img08}/>
+                <img src={Img09}/>
+                <img src={Img10}/>
+                <img src={Img11}/>
+                <img src={Img12}/>
+                <img src={Img13}/>
             </div>
         </section>
     );
